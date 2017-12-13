7122cbce0f0811ab22a068228ec65e6715d6b32d
Carlos <carlos.santos110@fatec.sp.gov.br>
2017-12-13 21:01:59 -0200

    Script para facilitar a conexao do Controle do PS3 no Linux

--- GUIwithYAD/calendario.sh
+++ GUIwithYAD/calendario.sh
@@ -4,16 +4,16 @@
 # mostra um calendário iniciando com o dia 20/05/2011
 # guarda o valor selecionar na variável $DATA
 
-DATA=$(\ 
-        yad --calendar \ 
-        --day=20 \ 
-        --month=5 \ 
-        --year=2011 \ 
-        --date-format=%d\/%m\/%Y \ 
-                --title=Calendario \ 
+DATA=$(\
+        yad --calendar \
+        --day=20 \
+        --month=5 \
+        --year=2011 \
+        --date-format=%d\/%m\/%Y \
+                --title=Calendario \
                 --center \ # disposição do diálogo na tela
-) 
+)
 
 # mostra um diálogo informando a $DATA selecionada
-yad --title="AVISO" \         --text="Você selecionou a data $DATA" 
+yad --title="AVISO" \         --text="Você selecionou a data $DATA"
 #.EOF
